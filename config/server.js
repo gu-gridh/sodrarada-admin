9d80a60cf1d7d5402304416c75aef5cc69641145
Arild Matsson <arild.matsson@gu.se>
2021-09-01 16:09:00 +0200

    Enable proxy

--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL'),
+  proxy: !!env('URL'),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '74a167311864e330b2df250ecfba8e58'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
