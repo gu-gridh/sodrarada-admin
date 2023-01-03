e48fdb5209f15bb5dbd1df3dd3df63c419d5ef9b
Arild Matsson <arild.matsson@gu.se>
2023-01-03 12:20:08 +0100

    Fix settings, env

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL'),
+  proxy: !!env('URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
